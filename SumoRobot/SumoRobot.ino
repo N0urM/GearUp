a493d26834ef5348fcfb0d2758723854bde3bb44
N0urM <NourMansour@users.noreply.github.com>
2018-12-05 00:03:41 +0200

    Initial approach to working mechanism

--- SumoRobot/SumoRobot.ino
+++ SumoRobot/SumoRobot.ino
@@ -1,23 +1,25 @@
 /* 
-0  Author   : Nourhan Mansour
+  Author       : Nourhan Mansour
+  contributors : Sherif El-Deeb & ElSaeed Ahmed
   Date     : 11/3/2018
   Function : Arduino C code implemened to make a robot with 2 ultasonic & 6 IRs 
              Kicks other robots out of the ring.
              The algorith used is divided into simple steps
                 1- scan the area for other opponents 
-                  • if found any --> attack with avg speed
+                  • if found any --> attack with avg speed if far away & with Max speed if close enough
                   • otherwise move forward and scan again
-                2- If you get anywhere near the edges of the ring --> turn with a small angle 
+                2- If you get anywhere near the edges of the ring --> turn with a small angle to avoid falling 
 */
 
 #include <Ultrasonic.h>
-Ultrasonic FrontUltrasonic(A0, A1);    // (Trigger , Echo)
+Ultrasonic FrontUltrasonic(10, 11);    // (Trigger , Echo)
 Ultrasonic BackUltrasonic(A2, A3);    // (Trigger , Echo)
+
 int distance=100, backDis, frontDis;
 int fspeed = 120 , mxspeed = 200 ,curveSpeed = 90;
 
-const byte rIN1 = 13 , rIN2 = 12 , rEN = 11;   // Right Motor
-const byte lIN1 = 9 , lIN2 = 8 , lEN = 10 ;    // Left Motor
+const byte rIN1 = 13 , rIN2 = 12 , rEN = A1;   // Right Motor
+const byte lIN1 = 9 , lIN2 = 8 , lEN = A0 ;    // Left Motor
 const byte FrontRightIR = 2 , FrontLeftIR = 4 , FrontMiddleIR = 3 ;     // Front IR sensors 
 const byte BackLeftIR = 5 , BackRightIR = 7 , BackMiddleIR = 6;         // Back IR sensors
 byte Frontright, Frontleft, Frontmiddle, Backleft, Backright, Backmiddle;
@@ -48,8 +50,26 @@ void TurnLeft();
 void MoveBackward(int s);
 void Stop();
 void makeAngle();
+
+// system starts here
 void loop() {
 
+  // each loop gets new IR readings to ensure avoiding obstacles 
+    Frontright=digitalRead(FrontRightIR);
+    Frontleft=digitalRead(FrontLeftIR);
+    Frontmiddle=digitalRead(FrontMiddleIR);
+  
+    Backleft=digitalRead(BackLeftIR);
+    Backright=digitalRead(BackRightIR);
+    Backmiddle=digitalRead(BackMiddleIR);
+
+    // no IR is active & we're away from the borders
+    if (Frontright==LOW &&Frontleft==LOW && Frontmiddle==LOW &&Backleft==LOW && Backright == LOW && Backmiddle == LOW ){
+      ScanArea();
+    }
+    else { // maybe close to an edge.
+      Avoid();
+    }
 
 
 }
@@ -102,25 +122,25 @@ void  TurnLeft () {
   
 void ScanArea() {
   
-  int ttl = 1;
+  int ttl = 1; // take values from 1 - 18 in order to make 180 degres and avoid moving in circles 
+  
   frontDis = FrontUltrasonic.read();
   backDis = BackUltrasonic.read();
  
  
- if (backDis >= distance && frontDis < distance)
+ if (backDis >= distance && frontDis < distance) // Enemy ahead
   {
-    //Enemy is in front.
+   
     AttackTarget('f');
   }
-  else if ( backDis < distance && frontDis >= distance ){
-    //Enemy is behind.
+  else if ( backDis < distance && frontDis >= distance ){  //Enemy is behind.
     AttackTarget('b');
   }
-  else
+  else  // Nothing found so keep searching
   {
-    MoveForward(fspeed);
-    delay (500); 
-    while (ttl < 18 && FrontUltrasonic.read() > distance && BackUltrasonic.read() > distance ) {
+    MoveForward(fspeed); // moveForward to avoid remaining in the same place.
+    delay (500); // keep moving for 0.5 s
+    while (ttl < 18 && FrontUltrasonic.read() > distance && BackUltrasonic.read() > distance ) { // make turns while searching
       ttl ++;
       makeAngle ();
      
@@ -130,18 +150,18 @@ void ScanArea() {
 }
 void makeAngle () {
       
- TurnRight ();
- delay (50);
- Stop();
+ TurnRight (); // right or left is the same
+ delay (50); // to be changed by try and error to make a small angle approx = 10 degrees
+ Stop(); // to give the sonics some space to capture readings.
  
   }
 
 void AttackTarget(char c) {
-  if (c=='f'){
-    if (FrontUltrasonic.read() <= distance / 2){
+  if (c=='f'){ // argument passed by the search function
+    if (FrontUltrasonic.read() <= distance / 2){ // IF the enemy is close we need to move fast to gain some tork.
         MoveForward (mxspeed);
       }
-      else {
+      else { // other wise we need to move on avg speed to make sure we don't run out of the borders.
         MoveForward(fspeed);
       }
   }else {
@@ -157,6 +177,7 @@ void AttackTarget(char c) {
 
 } 
 void Avoid(){
+  // Get reading from all IRS.
     Frontright=digitalRead(FrontRightIR);
     Frontleft=digitalRead(FrontLeftIR);
     Frontmiddle=digitalRead(FrontMiddleIR);
@@ -164,13 +185,11 @@ void Avoid(){
     Backleft=digitalRead(BackLeftIR);
     Backright=digitalRead(BackRightIR);
     Backmiddle=digitalRead(BackMiddleIR);
-  
-    if ( (Frontright==LOW && Frontleft==LOW && Frontmiddle==LOW) || (Backleft==LOW && Backright==LOW && Backmiddle==LOW) ){
-        TurnLeft(); // Or Right
-    }
-    else if ((Frontright==HIGH && Frontleft==LOW && Frontmiddle==LOW) || (Backleft==LOW && Backright==HIGH && Backmiddle==LOW) ){
+
+    if ((Frontright==HIGH && Frontleft==LOW && Frontmiddle==LOW) || (Backleft==LOW && Backright==HIGH && Backmiddle==LOW) ){
         TurnLeft();
     }
+    
     else if (( Frontright==LOW && Frontleft==LOW && Frontmiddle==HIGH) || (Backleft==LOW && Backright==LOW && Backmiddle==HIGH ) ){
         TurnRight(); // Or Left
     }
